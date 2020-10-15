fa8fa80879d60cc60a0b8ab4a7b65b4f8dee20d9
Test <31894630+otakuTz107@users.noreply.github.com>
2020-10-15 12:34:52 +0800

    Add files via upload

--- self/js/self.js
+++ self/js/self.js
@@ -5,7 +5,7 @@ var to={
 	e:function(cb){this.cb=cb;this.n.addEventListener("transitionend",this.cb)},
 	leftMove:function(num,narr){
 		this.n=gn(".lunboD:first-child"); 
-		if(this.n.classList.contains("lunboDMoving"))return; if(!num){num=1;narr=null}
+		if(this.n.classList.contains("lunboDMoving"))return; 
 		this.e((function(e){
 			this.n.removeEventListener("transitionend",this.cb); this.c("lunboDMoving"); this.n.style.marginLeft=0;
 			if(narr){var df=document.createDocumentFragment(); for(var i=0;i<narr.length;i++){df.appendChild(narr[i])}gn(".lunboD:last-child").parentNode.appendChild(df);  } else{ this.n.parentNode.appendChild(this.n) }
@@ -16,8 +16,8 @@ var to={
 	rightMove:function(num,narr){
 		var fn=gn(".lunboD:first-child");
 		if(fn.classList.contains("lunboDMoving"))return;
-		if(!num){num=1;narr=null} this.n=gn(".lunboD:last-child");
-		if(narr){
+		 this.n=gn(".lunboD:last-child");
+		if(narr&&narr.length>0){
 			var df=document.createDocumentFragment(); for(var i=0;i<narr.length;i++){df.appendChild(narr[i]);}
 			fn.parentNode.insertBefore(df,fn);this.n=gn(".lunboD:first-child")
 			}
@@ -25,8 +25,11 @@ var to={
 		this.e((function(e){this.c("lunboDMoving");this.n.removeEventListener("transitionend",this.cb)}).bind(this));
 		setTimeout((function(){this.n.style.marginLeft=0}).bind(this),14);
 	},
-	getSkipMoveArr:function(num,boo){
-		
-		
+	skipMove:function(num){
+		var boo=""; num>0?boo=true:boo=false;
+		num=Math.abs(num); if(num==0)return;
+		var narr=gn(".lunboD",true),arr=[]; if(narr.length-1<num)num=narr.length-1;
+		if(boo){ for(var i=0;i<num;i++)arr.push(narr[i]); to.leftMove(num,arr) }
+		else{ for(var i=num;i>0;i--)arr.push(narr[narr.length-i]); to.rightMove(num,arr) }
 	}
 };
